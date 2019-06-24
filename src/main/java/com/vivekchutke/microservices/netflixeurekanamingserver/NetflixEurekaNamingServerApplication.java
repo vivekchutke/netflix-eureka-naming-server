36c235218a6dd8e402a7ff327f8d7fa2d4c733d6
vivekchutke <vivekchutke@gmail.com>
2019-06-24 15:31:03 -0700

    MicroService Naming server which registers other micro service which helps in automatic scaling and unscaling of micro services

---
+++ src/main/java/com/vivekchutke/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -0,0 +1,22 @@
+package com.vivekchutke.microservices.netflixeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NetflixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+}
